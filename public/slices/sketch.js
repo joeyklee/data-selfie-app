1d71c66ab1a05bdae95ed8331e86ca879457fff0
joeyklee <jsphknglee@gmail.com>
2019-04-29 15:46:11 -0400

    added animation and hover effect

--- public/slices/sketch.js
+++ public/slices/sketch.js
@@ -1,12 +1,13 @@
 
 let myData;
-let img
+// let img
 function preload(){
     myData = loadJSON('/api');
 }
 
 function setup(){
-    createCanvas(800, 800)
+    // createCanvas(800, 800)
+    noCanvas();
     noLoop();
 }
 
@@ -16,13 +17,32 @@ function draw(){
     for(let p in myData){
         let item = myData[p];
         
-        img = createImg(item.image)
-        img.hide()
+        let img = createImg(item.image).parent('main')
+        // img.size(400, 400)
+        // img.elt.style.position = 'fixed';
+        const l = ceil(800/Object.keys(myData).length)
+        img.position(10 + p*l,20)
+        // img.elt.style.opacity = "0.5";
+        img.elt.id = p;
 
+        img.elt.addEventListener('mouseenter', (e) => {
+            document.querySelectorAll('img').forEach(thing => {
+                thing.style.zIndex = e.currentTarget.id;
+            })
+            e.currentTarget.style.zIndex = "999999"
+        })
+
+        
         // tint(255, 100)
-        const l = ceil(width/Object.keys(myData).length)
-        image(img, p*l,0, 800, 800)
+        
+        // image(img, p*l,0, 800, 800)
         
     }
+
+    document.querySelector('body').addEventListener('mouseout', (e) => {
+        document.querySelectorAll('img').forEach(thing => {
+            thing.style.zIndex = e.currentTarget.id;
+        })
+    })
 
 }
